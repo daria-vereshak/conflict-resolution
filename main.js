527d034d6bbc7aea2ef54cabf579daec4a9ea87e
taina91 <43749752+taina91@users.noreply.github.com>
2022-11-04 12:19:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-function bubbleSortConcept2(arr) {
-  let swapped;
-
-  do {
-    swapped = false;
-    console.log(arr);
-    arr.forEach((item, index) => {
-      if (item > arr[index + 1]) {
-        // Save the value to a variable so we don't lose it
-        let temp = item;
-        arr[index] = arr[index + 1];
-        arr[index + 1] = temp;
-        swapped = true;
-      }
-    })
-  } while (swapped);
-}
+const insertionSort = arr => {
+    for (let i = 1, l = arr.length; i < l; i++) {
+        const current = arr[i];
+        let j = i;
+        while (j > 0 && arr[j - 1] > current) {
+            arr[j] = arr[j - 1];
+            j--;
+        }
+        arr[j] = current;
+    }
+    return arr;
+};
